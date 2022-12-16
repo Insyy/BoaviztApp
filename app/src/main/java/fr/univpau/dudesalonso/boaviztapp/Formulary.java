6b042feac9e2ce0af91a359a324bd9c83a1f71ba
Ivan <ivanalonsodm@gmail.com>
2022-12-16 02:31:49 +0100

    huge ui improvement, used material design

--- app/src/main/java/fr/univpau/dudesalonso/boaviztapp/Formulary.java
+++ app/src/main/java/fr/univpau/dudesalonso/boaviztapp/Formulary.java
@@ -11,4 +11,8 @@ public class Formulary extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.formulary);
     }
+
+    private void setAutofilledContents(){
+
+    }
 }
