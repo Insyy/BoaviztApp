1c8b586acaa9750169968655ccfeb6d87fdb31fe
arn00sdq <59341836+arn00sdq@users.noreply.github.com>
2022-12-21 14:38:00 +0100

    refactor ServerImpactVisualizer

--- app/src/main/java/fr/univpau/dudesalonso/boaviztapp/ServerImpactVisualizer.java
+++ app/src/main/java/fr/univpau/dudesalonso/boaviztapp/ServerImpactVisualizer.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.progressindicator.LinearProgressIndicator;
@@ -16,10 +22,25 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 import fr.univpau.dudesalonso.boaviztapp.formulary.serverconfig.ServerConfiguration;
 
 public class ServerImpactVisualizer extends AppCompatActivity {
+
+    ArrayList<BarChart> barChartList = new ArrayList<>();
+
+    int[] colorClassArray1 = new int[]{
+            Color.rgb(1,139,140),
+            Color.rgb(203,230,255)};
+
+    int[] colorClassArray2 = new int[]{
+            Color.rgb(1,139,140),
+            Color.rgb(24,60,92),
+            Color.rgb(84,183,188),
+            Color.rgb(246,211,72),
+            Color.rgb(203,163,124),
+            Color.rgb(157,181,183)};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +48,27 @@ public class ServerImpactVisualizer extends AppCompatActivity {
         setContentView(R.layout.data_visualisation);
 
         setDarkMode();
-
         setBottomNavigationBarListener();
         setNavigationIconFocus();
 
-        ServerConfiguration config = (ServerConfiguration) getIntent().getSerializableExtra("serverConfiguration");
-        Log.d("ServerImpactVisualizer: configuration", config.toString());
+       // ServerConfiguration config = (ServerConfiguration) getIntent().getSerializableExtra("serverConfiguration");
+      /*  Log.d("ServerImpactVisualizer: configuration", config.toString());
+        Log.d("ServerImpactVisualizer", "Attempting to write value as JSON string");
+        Log.d("ServerImpactVisualizer", config.getAsJson());*/
 
-            Log.d("ServerImpactVisualizer", "Attempting to write value as JSON string");
-            Log.d("ServerImpactVisualizer", config.getAsJson());
+        BarChart layoutGlobalWarming = findViewById(R.id.global_warming);
+        BarChart layoutPrimaryEnergy = findViewById(R.id.primary_energy);
+        BarChart layoutRessExhausted = findViewById(R.id.ressource_exhausted);
+
+        barChartList.add(layoutGlobalWarming);
+        barChartList.add(layoutPrimaryEnergy);
+        barChartList.add(layoutRessExhausted);
+
+        int arraySize = barChartList.size();
+        for (int i = 0; i < arraySize; i ++) {
+            initLayoutChart(createBarData(i), barChartList.get(i));
+            initLayoutLegend(barChartList.get(i));
+        }
 
     }
 
@@ -110,6 +143,70 @@ public class ServerImpactVisualizer extends AppCompatActivity {
             return;
 
         //TODO FAIRE LES REQUETES PUIS MONTRER LES DONNEES
+    }
+
+    private BarData createBarData(int index){
+        BarDataSet barDataUp = new BarDataSet(dataValuesUp(index),"");
+        barDataUp.setColors(colorClassArray1);
+        barDataUp.setStackLabels(getResources().getStringArray(R.array.label_top_bar));
+        BarDataSet barDataDown = new BarDataSet(dataValuesDown(index),"");
+        barDataDown.setColors(colorClassArray2);
+        barDataDown.setStackLabels(getResources().getStringArray(R.array.label_bottom_bar));
+
+        BarData barData = new BarData(barDataUp,barDataDown);
+        barData.setBarWidth(5f);
+        barData.setDrawValues(false);
+
+        return barData;
+    }
+
+    private void initLayoutChart(BarData barData, BarChart barChart){
+        barChart.setData(barData);
+        barChart.setExtraOffsets(0f,5f,0f,5f);
+
+        barChart.getDescription().setEnabled(false);
+        //disable axis things
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.getXAxis().setDrawLabels(false);
+        barChart.getXAxis().setDrawAxisLine(false);
+
+        barChart.getAxisRight().setDrawGridLines(false);
+        barChart.getAxisRight().setDrawLabels(false);
+        barChart.getAxisRight().setDrawAxisLine(false);
+
+        barChart.getAxisLeft().setDrawGridLines(false);
+        barChart.getAxisLeft().setDrawLabels(false);
+        barChart.getAxisLeft().setDrawAxisLine(false);
+
+        //disable interaction
+        barChart.setTouchEnabled(false);
+        barChart.setScaleEnabled(false);
+        barChart.setDoubleTapToZoomEnabled(false);
+
+        //refresh
+        barChart.invalidate();
+    }
+
+    private void initLayoutLegend(BarChart barChart){
+        Legend legend = barChart.getLegend();
+        legend.setXEntrySpace(10f);
+        legend.setYEntrySpace(4f);
+        legend.setTextColor(Color.WHITE);
+        legend.setWordWrapEnabled(true);
+        legend.setTextColor(Color.DKGRAY);
+
+    }
+
+    private ArrayList<BarEntry> dataValuesUp(int index){
+        ArrayList<BarEntry> dataVals = new ArrayList<>();
+        dataVals.add(new BarEntry(4.95F , new float[]{1f,6f}));
+        return dataVals;
+    }
+
+    private ArrayList<BarEntry> dataValuesDown(int index){
+        ArrayList<BarEntry> dataVals = new ArrayList<>();
+        dataVals.add(new BarEntry(0 , new float[]{1f,0.1f,0.8f,1.1f,0.2f,3.8f}));
+        return dataVals;
     }
 
 }
